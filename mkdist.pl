d657df3302654338e5c43d6caabbeda76a02c341
AAHAZRED <451@gmx.eu>
2025-01-03 12:35:33 +0100

    added mkdist

---
+++ mkdist.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+
+use File::Spec::Functions;
+use File::Path;
+use File::Find;
+use FindBin;
+
+system("make distdir") == 0 or die;
+my @Entries;
+foreach my $e (<*-*>) {
+  next unless -d $e;
+  next unless $e =~ /\w+(?:-\w+)*-v?\d+(?:\.\d+){1,2}$/;
+  push(@Entries, $e);
+}
+if (!@Entries) {
+  die("No matching directories");
+} elsif (@Entries > 1) {
+  die("Found multiple matching directories: @Entries");
+}
+my $DistVName = $Entries[0];
+my $Filter = catfile($FindBin::Bin, 'pm_filter_simple.pl');
+find({ no_chdir => 1,
+       wanted   => sub { if (/\.pm$/ && -f $_) { system("perl $Filter $_") == 0 or die;} }
+     },
+     catdir($DistVName, 'lib'));
+system("tar cvf $DistVName") == 0 or die;
+rmtree($DistVName);
+system("gzip -9f $DistVName.tar") == 0 or die;
+print("Created $DistVName.tar.gz");
+
